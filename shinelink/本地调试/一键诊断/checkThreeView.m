895de72b14eaeb3e14ff91883076cd229a049711
sky <328657662@qq.com>
2018-02-11 17:48:11 +0800

    v2.11.2

--- shinelink/本地调试/一键诊断/checkThreeView.m
+++ shinelink/本地调试/一键诊断/checkThreeView.m
@@ -61,6 +61,8 @@
 @property (assign, nonatomic) int progressNum;
 @property(nonatomic,strong)usbToWifiDataControl*changeDataValue;
 
+@property (strong, nonatomic)UIView*view3;
+
 @end
 
 @implementation checkThreeView
@@ -99,7 +101,7 @@
     }
     
     if (_isThreeViewEnable) {
-         _isThreeViewH=everyLalbeH+300*HEIGHT_SIZE+120*HEIGHT_SIZE;
+         _isThreeViewH=everyLalbeH+270*HEIGHT_SIZE+120*HEIGHT_SIZE;
         
         [self initThreeView];
         
@@ -305,6 +307,7 @@
             
             _barRightArray=@[[NSString stringWithFormat:@"%d",[_changeDataValue changeOneRegister:data registerNum:0]],[NSString stringWithFormat:@"%d",[_changeDataValue changeOneRegister:data registerNum:11]],[NSString stringWithFormat:@"%d",[_changeDataValue changeOneRegister:data registerNum:22]]];
             
+            [self removeTheNotification];
             [self initThreeView];
             [self goToReadAllChart];
         }
@@ -325,10 +328,18 @@
 
             _zuKangArray=@[[NSString stringWithFormat:@"%d",[_changeDataValue changeOneRegister:data registerNum:33]],[NSString stringWithFormat:@"%d",[_changeDataValue changeOneRegister:data registerNum:34]],[NSString stringWithFormat:@"%d",[_changeDataValue changeOneRegister:data registerNum:35]]];
             
+                 [self removeTheNotification];
             [self initFourView];
         }
     }
     
+    
+}
+
+
+-(void)removeTheNotification{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:@"TcpReceiveOneKey" object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:@"TcpReceiveOneKeyFailed" object:nil];
     
 }
 
@@ -406,6 +417,18 @@
 
 -(void)initThreeView{
 
+    UIView* view1=[[UIView alloc]initWithFrame:CGRectMake(0,lastH+_isOneViewH+_isTwoViewH, SCREEN_Width, everyLalbeH)];
+    view1.backgroundColor=[UIColor whiteColor];
+    [_scrollViewAll addSubview:view1];
+    
+    UILabel *titleLable = [[UILabel alloc]initWithFrame:CGRectMake(0,0, SCREEN_Width, everyLalbeH)];
+    titleLable.textColor =MainColor;
+    titleLable.textAlignment=NSTextAlignmentCenter;
+    titleLable.text=@"电网线路图(THDV)";
+    titleLable.font = [UIFont systemFontOfSize:14*HEIGHT_SIZE];
+    [view1 addSubview:titleLable];
+    
+    
     if (!_barDataSource2) {
         _barDataSource2=[NSMutableArray arrayWithArray:@[@[@"0",@"0",@"0"],@[@"0",@"0",@"0"],@[@"0",@"0",@"0"],@[@"0",@"0",@"0"],@[@"0",@"0",@"0"],@[@"0",@"0",@"0"],@[@"0",@"0",@"0"],@[@"0",@"0",@"0"],@[@"0",@"0",@"0"],@[@"0",@"0",@"0"]]];
     }
@@ -449,7 +472,7 @@
     _Xtitles2=@[@"1",@"3",@"5",@"7",@"9",@"11",@"13",@"15",@"17",@"19",];
  
     
-        _barChartView2 = [[MCBarChartView alloc] initWithFrame:CGRectMake(0, lastH+_isOneViewH+_isTwoViewH, ScreenWidth,  _isThreeViewH-150*HEIGHT_SIZE)];
+        _barChartView2 = [[MCBarChartView alloc] initWithFrame:CGRectMake(0, lastH+_isOneViewH+_isTwoViewH+15*HEIGHT_SIZE, ScreenWidth+everyLalbeH,  _isThreeViewH-120*HEIGHT_SIZE-everyLalbeH)];
         _barChartView2.tag = 222;
         _barChartView2.dataSource = self;
         _barChartView2.delegate = self;
@@ -465,9 +488,83 @@
     
     [_barChartView2 reloadDataWithAnimate:YES];
     
+    if (_barRightArray.count>0) {
+        if (_view3) {
+            [_view3 removeFromSuperview];
+            _view3=nil;
+        }
+        _view3 = [[UIView alloc]initWithFrame:CGRectMake(0, lastH+_isOneViewH+_isTwoViewH+_isThreeViewH-120*HEIGHT_SIZE+5*HEIGHT_SIZE,ScreenWidth,110*HEIGHT_SIZE)];
+        _view3.backgroundColor=[UIColor whiteColor];
+        [_scrollViewAll addSubview:_view3];
+        
+        NSArray *color1Array=@[COLOR(208, 107, 107, 1),COLOR(217, 189, 60, 1),COLOR(85, 207, 85, 1)];
+        NSArray *nameArray=@[@"Total(R)",@"Total(S)",@"Total(T)",];
+        
+        float imageViewH=10*HEIGHT_SIZE; float Wk=5*NOW_SIZE;
+        float imageViewx=30*NOW_SIZE;
+        for (int i=0; i<color1Array.count; i++) {
+            
+            //        UIView* view21=[[UIView alloc]initWithFrame:CGRectMake(0,everyLalbeH*(i+1), SCREEN_Width/2, everyLalbeH)];
+            //        view21.backgroundColor=[UIColor whiteColor];
+            //        [view2 addSubview:view21];
+            
+            UIButton *button1 = [UIButton buttonWithType:UIButtonTypeCustom];
+            button1.frame = CGRectMake(0,everyLalbeH*(i), SCREEN_Width/2, everyLalbeH);
+            [button1 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+            [button1 setTitleColor:COLOR(242, 242, 242, 1) forState:UIControlStateHighlighted];
+            button1.tag = 8000+i;
+            button1.backgroundColor=[UIColor whiteColor];
+            button1.selected=YES;
+            button1.titleLabel.font=[UIFont systemFontOfSize: 14*HEIGHT_SIZE];
+            [button1 setTitle:@"" forState:UIControlStateNormal];
+            [button1 addTarget:self action:@selector(buttonForNum:) forControlEvents:UIControlEventTouchUpInside];
+            [_view3 addSubview:button1];
+            
+            UIView* imageView=[[UIView alloc]initWithFrame:CGRectMake(imageViewx,(everyLalbeH-imageViewH)/2, imageViewH, imageViewH)];
+            imageView.backgroundColor=color1Array[i];
+            imageView.tag=8100+i;
+            [button1 addSubview:imageView];
+            
+            Lable11x=imageViewx+imageViewH+Wk*2;
+            UILabel *Lable11 = [[UILabel alloc]initWithFrame:CGRectMake(Lable11x, 0,ScreenWidth/2-Lable11x,everyLalbeH)];
+            Lable11.textColor =COLOR(102, 102, 102, 1);
+            Lable11.textAlignment=NSTextAlignmentLeft;
+            Lable11.adjustsFontSizeToFitWidth=YES;
+            Lable11.text=nameArray[i];
+            Lable11.tag=8200+i;
+            Lable11.font = [UIFont systemFontOfSize:12*HEIGHT_SIZE];
+            [button1 addSubview:Lable11];
+            
+            UIView* view22=[[UIView alloc]initWithFrame:CGRectMake(SCREEN_Width/2,everyLalbeH*(i), SCREEN_Width/2, everyLalbeH)];
+            view22.backgroundColor=[UIColor whiteColor];
+            [_view3 addSubview:view22];
+            
+            //  UILabel *Lable22 = [[UILabel alloc]initWithFrame:CGRectMake((ScreenWidth/2-lable1Size2.width)/2-Wk, 0,ScreenWidth/2-Lable11x,everyLalbeH)];
+            UILabel *Lable22 = [[UILabel alloc]initWithFrame:CGRectMake(0, 0,ScreenWidth/2,everyLalbeH)];
+            Lable22.textColor =COLOR(102, 102, 102, 1);
+            Lable22.textAlignment=NSTextAlignmentCenter;
+            Lable22.adjustsFontSizeToFitWidth=YES;
+            Lable22.text=_barRightArray[i];
+            Lable22.tag=7000+i;
+            Lable22.font = [UIFont systemFontOfSize:12*HEIGHT_SIZE];
+            [view22 addSubview:Lable22];
+            
+            
+        }
+        
+        UIView* view0=[[UIView alloc]initWithFrame:CGRectMake(0,lastH+_isOneViewH+_isTwoViewH+_isThreeViewH-everyModelKongH, SCREEN_Width, everyModelKongH)];
+        view0.backgroundColor=COLOR(242, 242, 242, 1);
+        [_scrollViewAll addSubview:view0];
+    }
+   
+    
+    
+    
 }
 
-
+-(void)buttonForNum:(UIButton*)button{
+    
+}
 
 -(void)initFourView{
     
